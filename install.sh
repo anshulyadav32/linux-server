cb61f987e19e16d9a6f8d9e1e37234d9a336f28d
Anshul Yadav <{public_email}>
2025-09-11 01:46:06 +0530

    Add firewall, ssl, and backup modules to install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,31 @@
+echo "Installing Firewall module..."
+sudo su -c "bash $(pwd)/modules/firewall/install.sh" &
+firewall_pid=$!
+
+echo "Installing SSL module..."
+sudo su -c "bash $(pwd)/modules/ssl/install.sh" &
+ssl_pid=$!
+
+echo "Installing Backup module..."
+sudo su -c "bash $(pwd)/modules/backup/install.sh" &
+backup_pid=$!
+
+wait $firewall_pid
+firewall_status=$?
+wait $ssl_pid
+ssl_status=$?
+wait $backup_pid
+backup_status=$?
 
 
 #!/bin/bash
 
 
+echo "Installing Database module..."
+sudo su -c "bash $(pwd)/modules/database/install.sh" &
+database_pid=$!
+wait $webserver_pid
+wait $database_pid
 echo "Installing Webserver module..."
 sudo su -c "bash $(pwd)/modules/webserver/install.sh" &
 webserver_pid=$!
@@ -11,14 +34,20 @@ echo "Installing Database module..."
 sudo su -c "bash $(pwd)/modules/database/install.sh" &
 database_pid=$!
 
+echo "Installing DNS module..."
+sudo su -c "bash $(pwd)/modules/dns/install.sh" &
+dns_pid=$!
+
 wait $webserver_pid
 webserver_status=$?
 wait $database_pid
 database_status=$?
+wait $dns_pid
+dns_status=$?
 
-if [ $webserver_status -eq 0 ] && [ $database_status -eq 0 ]; then
-	echo "Both installations completed successfully."
+if [ $webserver_status -eq 0 ] && [ $database_status -eq 0 ] && [ $dns_status -eq 0 ] && [ $firewall_status -eq 0 ] && [ $ssl_status -eq 0 ] && [ $backup_status -eq 0 ]; then
+	echo "All installations completed successfully."
 else
-	echo "One or both installations failed."
+	echo "One or more installations failed."
 	exit 1
 fi
