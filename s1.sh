c3aef375d97035da63c74bba7563e7832ec28544
Anshul Yadav <{public_email}>
2025-09-11 01:43:47 +0530

    Improve s1.sh: pull if repo exists, clone if not

--- s1.sh
+++ s1.sh
@@ -2,8 +2,15 @@
 # Remote installer for linux-server
 set -e
 
-echo "Cloning linux-server repository..."
-git clone https://github.com/anshulyadav32/linux-server.git
-cd linux-server
+
+if [ -d "linux-server" ]; then
+	echo "linux-server directory exists. Pulling latest changes..."
+	cd linux-server
+	git pull origin main
+else
+	echo "Cloning linux-server repository..."
+	git clone https://github.com/anshulyadav32/linux-server.git
+	cd linux-server
+fi
 echo "Running install.sh..."
 sudo bash install.sh
