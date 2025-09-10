27bce9a310e3b48e863fce88060f5ca50e98ea33
Anshul Yadav <{public_email}>
2025-09-11 00:16:56 +0530

    Run webserver and database installs in parallel in install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,12 @@
 
 #!/bin/bash
 
+
 echo "Installing Webserver module..."
-sudo su -c "bash $(pwd)/modules/webserver/install.sh"
+sudo su -c "bash $(pwd)/modules/webserver/install.sh" &
+
+echo "Installing Database module..."
+sudo su -c "bash $(pwd)/modules/database/install.sh" &
+
+wait
+echo "Both installations completed."
